62b30f2513ff9190d60778af5d718c2d5704dd88
Fenton Travers <fenton.travers@gmail.com>
2017-10-27 15:55:31 -0700

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fentontravers/reframe-websocket "0.1.3-SNAPSHOT"
+(defproject fentontravers/reframe-websocket "0.1.3"
   :description "Wraps basic reframe, websocket, and transit capabilities for communicating to a server."
   :url "https://github.com/ftravers/reframe-websocket"
   :license {:name "Eclipse Public License"
